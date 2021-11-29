3ec886d453d2fd6890f305a4f17e1dd22c991507
Lorenzo Fumi <lorenzofumi@gmail.com>
2021-11-29 12:02:02 +0100

    Added libraries: mongodb, dotenv, added mongodb client in the code

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var express = require('express')
+var dotenv = require('dotenv') // https://www.npmjs.com/package/dotenv
 var app = express()
+const {
+    MongoClient
+} = require("mongodb");
+
+const connectionString = process.env.ATLAS_URI;
+const client = new MongoClient(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+client.connect(function (err, db) {
+    if (err || !db) {
+        console.error(err);
+    }
+
+    dbConnection = db.db("sample_airbnb");
+    console.log("Successfully connected to MongoDB.");
+});
+
 
 var annunci = require('./api/annunci')
 
