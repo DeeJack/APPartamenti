fc6b2c20fb32a979b2f14b4c450ce28bf97b1dc8
Lorenzo <lorenzofumi@gmail.com>
2021-12-03 18:40:39 +0100

    added post, put, delete for 'annunci', the data to insert in the database needs to be defined

--- api/annunci.js
+++ api/annunci.js
@@ -23,15 +23,82 @@ router.get('/', (request, response) => {
 })
 
 router.get('/:id', (request, response) => {
-    console.log(request)
-    response.json({
-        id: request.params["id"]
-    })
+    const dbConnect = dbo.getDb();
+
+    dbConnect
+        .collection("annunci")
+        .findOne({
+            _id: request.params['id']
+        }).limit(50)
+        .toArray(function (err, result) {
+            if (err) {
+                res.status(400).send("Error fetching listings!");
+            } else {
+                res.json(result);
+            }
+        });
 })
 
 router.post('/', urlencodedParser, (request, response) => {
     console.log('Got body:', req.body);
     console.log(connection)
+    const dbConnect = dbo.getDb();
+
+    const matchDocument = {
+        // dati annuncio
+    };
+
+    dbConnect
+        .collection("matches")
+        .insertOne(matchDocument, function (err, result) {
+            if (err) {
+                response.status(400).send("Error inserting matches!");
+            } else {
+                console.log(`Added a new match with id ${result.insertedId}`);
+                response.status(204).send();
+            }
+        });
+})
+
+router.put('/', (request, response) => {
+    const dbConnect = dbo.getDb();
+    const listingQuery = {
+        _id: req.body.id
+    };
+    const updates = {
+        $inc: {
+            likes: 1
+        }
+    };
+
+    dbConnect
+        .collection("listingsAndReviews")
+        .updateOne(listingQuery, updates, function (err, _result) {
+            if (err) {
+                response.status(400).send(`Error updating likes on listing with id ${listingQuery.id}!`);
+            } else {
+                response.send('updated')
+                console.log("1 document updated");
+            }
+        });
+})
+
+router.delete('/:id', (request, response) => {
+    const dbConnect = dbo.getDb();
+    const listingQuery = {
+        // ID
+    };
+
+    dbConnect
+        .collection("listingsAndReviews")
+        .deleteOne(listingQuery, function (err, _result) {
+            if (err) {
+                response.status(400).send(`Error deleting listing with id ${listingQuery.listing_id}!`);
+            } else {
+                response.redirect('/')
+                console.log("1 document deleted");
+            }
+        });
 })
 
 module.exports = router;
